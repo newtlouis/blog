b3533386ace2f5f863dfffdbd327e1bd1ab208b5
Louis <louis.m.newton@hotmail.fr>
2021-08-29 14:25:29 +0200

    structure page accueil et contact

---
+++ contact.php
@@ -0,0 +1,6 @@
+<?php require 'header.php'; ?>
+<br><br><br>
+<h2>Nous contacter</h2>
+<p>Blablabla</p>
+
+<?php require 'footer.php'; ?>
---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer class="footer mt-auto py-3 bg-light">
+  <div class="container">
+    <span class="text-muted">Place sticky footer content here.</span>
+  </div>
+</footer>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+      
+  </body>
+</html>
