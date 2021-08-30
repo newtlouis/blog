d2f7d845259d25b25dafd48967daa98b9d04c6dc
Louis <louis.m.newton@hotmail.fr>
2021-08-30 14:00:37 +0200

    JEU composer et prix glace

--- fonctions.php
+++ fonctions.php
@@ -1,6 +1,7 @@
 
 <?php
 
+// HEADER
 function nav_item (string $link, string $titre, string $linkClass = ''): string
  {
  $class = 'nav-link';
@@ -24,6 +25,35 @@ function nav_menu ( string $linkClass = ''):string {
         nav_item('/contact.php', 'Contact', $linkClass) .
         nav_item('/jeu.php', 'Jeu', $linkClass);
 }
+
+
+
+// PRIX DE LA GLACE
+
+// insertion checkbox
+function checkbox( string $name , string $value , array $data): string {
+    $attribute = '';
+    if (isset($_GET[$name]) && in_array($value , $data[$name])) {
+        $attribute .= "checked";
+    }
+    return <<<HTML
+        <input type="checkbox" name="{$name}[]" value="$value" $attribute>
+        HTML;
+}
+
+// insertion radio
+function radio( string $name , string $value , array $data): string {
+    $attribute = '';
+    if (isset($_GET[$name]) && $value === $data[$name]) {
+        $attribute .= "checked";
+    }
+    return <<<HTML
+        <input type="radio" name="$name" value="$value" $attribute>
+        HTML;
+}
+
+// Calcul du prix de la glace
+
 
 ?>
 
--- jeu.php
+++ jeu.php
@@ -8,8 +8,49 @@ $success = null;
 $result = null;
 $styleResult = null;
 $value = null;
+
 $listParfumsItem = null;
 $listParfums = null ;
+
+$parfums = [
+    'vanille' => 2,
+    'fraise' => 3,
+    'chocolat' => 2,
+    'pistache' => 3,
+    'mangue' => 2,
+];
+$cornets = [
+    'pot' => 0.5,
+    'cornet' => 1,
+];
+$supplements = [
+    'chantilly' => 1,
+    'pépites de chocolat' => 1.5,
+];
+$ingredients = [];
+$total = 0;
+
+foreach(['parfum' , 'supplement' , 'cornet'] as $name){
+    if (isset($_GET[$name])){
+        $liste = $name . "s";
+        $choix = $_GET[$name];
+        if (is_array($choix)){
+            foreach($choix as $value){
+                if (isset($$liste[$value])){
+                    $ingredients[] = $value;
+                    $total += $$liste[$value];
+                }
+            }
+        }
+        else {
+            if (isset($$liste[$value])){
+                $ingredients[] = $value;
+                $total += $$liste[$value];
+            }
+        }
+    }
+}
+
 
 // Le chiffre est trop haut ou trop bas ou correct ?
 if (isset($_GET['chiffre'])){
@@ -46,6 +87,10 @@ $listParfums = "Voici votre glace " . $listParfumsItem;
 
 ?>
 
+
+
+
+
 <!-- HTML -->
 
 <div class="section" style="margin: 60px 30px">
@@ -59,6 +104,8 @@ $listParfums = "Voici votre glace " . $listParfumsItem;
     </form>
     <div class="<?= $styleResult ?>"><?= $result ?></div>
 
+    <br><br>
+    
     <!-- Methode GET pour avoir les données dans l'url, méthode POST pour les cacher(safe). -->
     <h2>Quels sont vos parfums de glace préférés ?</h2>
     <form action="/jeu.php" method="post">
@@ -71,6 +118,71 @@ $listParfums = "Voici votre glace " . $listParfumsItem;
         <button type="submit">Commander</button>
     </form>
     <div class="listParfum"> <?= $listParfums ?></div>
+    
+    <br><br>
+
+    <h2>Maintenant, payer votre glace €€€</h2>
+    <div class="row">
+        <div class="col-md-4">
+            <h5>Votre glace</h5>
+            <div class="">
+                <ul>
+                    <?php foreach($ingredients as $ingredient): ?>
+                        <li> <?= $ingredient ?> </li>
+                    <?php endforeach ?>
+                </ul>
+            </div>
+            <div class="">
+                Au prix de <?= $total ?> €
+            </div>
+        </div>
+        <div class="col-md-4">
+            <form action="/jeu.php" method="get">
+                <!-- liste parfum -->
+                <h5>Choisissez vos parfums:</h5>
+                <?php foreach($parfums as $parfum => $prix): ?>
+                    <div class="checkbox">
+                        <label>
+                            <?= checkbox('parfum' , $parfum , $_GET) ?>
+                            <?= $parfum ?> - <?= $prix ?> €
+                        </label>
+                    </div>
+                <?php endforeach ?>
+                
+                <!-- liste cornet -->
+                <br>
+                <h5>Choisissez votre cornet:</h5>
+                <?php foreach($cornets as $cornet => $prix): ?>
+                    <div class="checkbox">
+                        <label>
+                            <?= radio('cornet' , $cornet , $_GET) ?>
+                            <?= $cornet ?> - <?= $prix ?> €
+                        </label>
+                    </div>
+                <?php endforeach ?>
+
+                <!-- liste supplément -->
+                <br>
+                <h5>Choisissez vos suppléments:</h5>
+                <?php foreach($supplements as $supplement => $prix): ?>
+                    <div class="checkbox">
+                        <label>
+                            <?= checkbox('supplement' , $supplement , $_GET) ?>
+                            <?= $supplement ?> - <?= $prix ?> €
+                        </label>
+                    </div>
+                <?php endforeach ?>
+
+                <br>
+
+                <button type="submit">Donnez moi le prix svp</button>
+                
+            </form>
+        </div>
+    </div>
+
+    
+
 
 </div>
 
