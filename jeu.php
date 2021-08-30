0455b48dcbe8fd791ff851781e94204dc036084d
Louis <louis.m.newton@hotmail.fr>
2021-08-30 11:17:11 +0200

    JEU séparation logique et html

--- jeu.php
+++ jeu.php
@@ -1,23 +1,45 @@
 <!-- FOOTER -->
-<?php require 'header.php'?>
-<?php $devinette = 3 ?>
+<?php require 'header.php';
+$devinette = 25;
+$fail = null;
+$success = null;
+$result = null;
+$styleResult = null;
+$value = null;
+
+if (isset($_GET['chiffre'])){
+    if($_GET['chiffre'] > $devinette){
+        $fail = "Trop haut !";
+    }
+    elseif ($_GET['chiffre'] < $devinette){
+        $fail = "Trop bas !";
+    }
+    else{
+        $success="Félicitation ! J'ai bien <strong>$devinette</strong> ans.";
+    }
+    $value = htmlentities($_GET['chiffre']);
+}
+
+if($success){
+    $result = $success;
+    $styleResult = 'alert alert-success';
+}
+else{
+    $result = $fail;
+    $styleResult = 'alert alert-danger';
+}
+
+?>
 
 <div class="section" style="margin: 60px 30px">
+    <h2>Deviner mon age</h2>
     <form action="/jeu.php" method="get">
     <!-- name dans input sert pour que php récupère la donnée -->
-        <input type="number" name="chiffre" placeholder="entre 0 et 10" value="<?php if(isset($_GET['chiffre'])) { echo htmlentities($_GET['chiffre']);}?>">
+        <input type="number" name="chiffre" placeholder="entre 0 et 100" value="<?= $value?>">
         <button type="submit">Deviner</button>
     </form>
 
-    <?php if (isset($_GET['chiffre'])):?>
-        <?php if($_GET['chiffre'] > $devinette): ?>
-            Trop haut !
-        <?php elseif ($_GET['chiffre'] < $devinette): ?>
-            Trop bas ..
-        <?php else: ?>
-            Bravo vous avez trouvé le bon chiffre !
-        <?php endif ?>
-    <?php endif ?>
+    <div class="<?= $styleResult ?>"><?= $result ?></div>
 
 
 </div>
