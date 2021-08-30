3cff0809019ef262edeadef2afa622f44bda5503
Louis <louis.m.newton@hotmail.fr>
2021-08-30 10:44:09 +0200

    never trust user in input

--- jeu.php
+++ jeu.php
@@ -5,22 +5,20 @@
 <div class="section" style="margin: 60px 30px">
     <form action="/jeu.php" method="get">
     <!-- name dans input sert pour que php récupère la donnée -->
-        <input type="number" name="chiffre" placeholder="entre 0 et 10" value="<?= (int)$_GET['chiffre']?>">
+        <input type="number" name="chiffre" placeholder="entre 0 et 10" value="<?php if(isset($_GET['chiffre'])) { echo htmlentities($_GET['chiffre']);}?>">
         <button type="submit">Deviner</button>
     </form>
 
-
-    <?php if($_GET['chiffre'] > $devinette): ?>
-        Trop haut !
-    <?php elseif ($_GET['chiffre'] < $devinette): ?>
-        Trop bas ..
-    <?php else: ?>
-        Bravo vous avez trouvé le bon chiffre !
+    <?php if (isset($_GET['chiffre'])):?>
+        <?php if($_GET['chiffre'] > $devinette): ?>
+            Trop haut !
+        <?php elseif ($_GET['chiffre'] < $devinette): ?>
+            Trop bas ..
+        <?php else: ?>
+            Bravo vous avez trouvé le bon chiffre !
+        <?php endif ?>
     <?php endif ?>
 
-<pre>
-    <?php $_GET ?>
-</pre>
 
 </div>
 
